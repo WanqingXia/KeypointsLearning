9f3cc855e90024735f321eef847869b614b02355
Wanqing <wxia612@aucklanduni.ac.nz>
2022-05-23 00:18:02 +1200

    generate_ycb finished

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 python calc_box.py
 2. run generate.py to sample camera points and generate images with
 blenderproc run generate.py
-if the previous command does not work, try python ./../cli.py run generate.py
+if the previous command does not work, try python ./../cli.py run generate_ycb.py
 3. run calc_angles.py to calculate the angles between a point and it's neigbouring points with
 python calc_angles.py
 4. run sift.py to calculate and filter 2D keypoints
--- generate_ycb.py
+++ generate_ycb.py
@@ -5,6 +5,8 @@ import cv2
 import h5py
 import shutil
 import scipy.io
+import glob
+import png
 
 # functions for debug, only uncomment this when debugging is needed
 # import debugpy
@@ -12,73 +14,83 @@ import scipy.io
 # debugpy.wait_for_client()
 
 
-
-
 def render(folder_name, model_paths, out_folder, count):
     bproc.init()
+
+    # create output folder
+    out_subfolder = os.path.join(out_folder,folder_name.split("/")[-1] )
+    if os.path.exists(out_subfolder):
+        shutil.rmtree(out_subfolder)
+    os.mkdir(out_subfolder)
 
     if count == 0:
         bproc.renderer.enable_depth_output(activate_antialiasing=False)
 
-    mat = scipy.io.loadmat('000001-meta.mat')
+    mat_files = sorted(glob.glob(os.path.join(folder_name, "*.mat")))
 
-    # load the objects into the scene
-    obj1 = bproc.loader.load_obj("textured.obj")
-    # define a light and set its location and energy level
-    light = bproc.types.Light()
-    light.set_location(mat['rotation_translation_matrix'][:, 3].flatten())
-    light.set_type("POINT")
-    light.set_energy(100)
+    for num, name in enumerate(mat_files):
+        # read mat file
+        mat = scipy.io.loadmat(name)
 
-    cam2world = np.asarray(mat['rotation_translation_matrix'])
+        # define a light and set its location and energy level
+        light = bproc.types.Light()
+        light.set_location(mat['rotation_translation_matrix'][:, 3].flatten())
+        light.set_type("POINT")
+        light.set_energy(80)
 
-    # Change coordinate frame of transformation matrix from OpenCV to Blender coordinates
-    cam2world = np.vstack((cam2world, [0, 0, 0, 1.0]))
+        # load the objects into the scene
+        if num == 0:
+            objs = []
+            for obj_num in mat["cls_indexes"]:
+                objs.append(bproc.loader.load_obj(model_paths[int(obj_num)-1]))
 
-    for obj in obj1:
-        pose = np.asarray(mat['poses'][:, :, 0])
-        pose_n = np.vstack((pose, [0, 0, 0, 1.0]))
-        tran = np.matmul(cam2world, pose_n[:, 3])
-        rot =  np.matmul(cam2world[0:3,0:3], pose[:, 0:3])
-        pose_n[:, 3] = tran
-        pose_n[0:3, 0:3] = rot
-        obj.set_local2world_mat(pose_n)
-        print(pose_n)
+        
+        cam2world = np.asarray(mat['rotation_translation_matrix'])
+        cam2world = np.vstack((cam2world, [0, 0, 0, 1.0]))
 
-    # for obj in obj2:
-    #     arr = np.asarray(mat['poses'][:, :, 1])
-    #     rot = arr[:, 0:3]
-    #     arr = np.vstack((arr, [0, 0, 0, 1.0]))
-    #     what = np.matmul(arr[:, 3], cam2world.transpose())
-    #     rot =  np.matmul(cam2world[0:3,0:3],rot)
-    #     arr[:, 3] = what.transpose()
-    #     arr[0:3, 0:3] = rot
-    #     obj.set_local2world_mat(arr)
-    #
-    # for obj in obj3:
-    #     arr = np.asarray(mat['poses'][:, :, 2])
-    #     rot = arr[:, 0:3]
-    #     arr = np.vstack((arr, [0, 0, 0, 1.0]))
-    #     what = np.matmul(arr[:, 3], cam2world.transpose())
-    #     rot =  np.matmul(cam2world[0:3,0:3],rot)
-    #     arr[:, 3] = what.transpose()
-    #     arr[0:3, 0:3] = rot
-    #     obj.set_local2world_mat(arr)
+        for count, obj in enumerate(objs):
+            for object in obj:
+                pose = np.asarray(mat['poses'][:, :, count])
+                pose_n = np.vstack((pose, [0, 0, 0, 1.0]))
+                # Calculate translation in world
+                pose_n[:, 3] = np.matmul(cam2world, pose_n[:, 3])
+                # Calculate rotation in world
+                pose_n[0:3, 0:3] = np.matmul(cam2world[0:3,0:3], pose[:, 0:3])
+                object.set_local2world_mat(pose_n)
 
-    bproc.camera.set_intrinsics_from_K_matrix(mat['intrinsic_matrix'], 640, 480)
-    # Set camera pose via cam-to-world transformation matrix
+        # Set camera intrinsics with K matrix
+        bproc.camera.set_intrinsics_from_K_matrix(mat['intrinsic_matrix'], 640, 480)
+        # Read cam-to-world transformation matrix (extrincis)
+        cam2world = np.asarray(mat['rotation_translation_matrix'])
+        # Change coordinate frame of transformation matrix from OpenCV to Blender coordinates
+        cam2world = bproc.math.change_source_coordinate_frame_of_transformation_matrix(cam2world, ["X", "-Y", "-Z"])
+        cam2world = np.vstack((cam2world, [0, 0, 0, 1.0]))
+        bproc.camera.add_camera_pose(cam2world)
 
-    cam2world = np.asarray(mat['rotation_translation_matrix'])
-    # Change coordinate frame of transformation matrix from OpenCV to Blender coordinates
-    cam2world = bproc.math.change_source_coordinate_frame_of_transformation_matrix(cam2world, ["X", "-Y", "-Z"])
-    cam2world = np.vstack((cam2world, [0, 0, 0, 1.0]))
-    bproc.camera.add_camera_pose(cam2world)
-    bproc.renderer.enable_depth_output(activate_antialiasing=False)
+        # render the whole pipeline
+        data = bproc.renderer.render()
+        # write the data to a .hdf5 container
+        bproc.writer.write_hdf5(os.path.join(out_subfolder, str(num)), data)
 
-    # render the whole pipeline
-    data = bproc.renderer.render()
-    # write the data to a .hdf5 container
-    bproc.writer.write_hdf5("./output", data)
+        # reset the scene (clear the camera and light)
+        bproc.utility.reset_keyframes()
+
+        with h5py.File(os.path.join(out_subfolder, (str(num) + '/0.hdf5')),'r') as h5f:
+            print(os.path.join(out_subfolder, (str(num) + '/0.hdf5')))
+            colour_file = os.path.join(out_subfolder, (name.split("/")[-1].split("-")[0]+ "-color.png"))
+            print(colour_file)
+
+            depth_file = os.path.join(out_subfolder, (name.split("/")[-1].split("-")[0]+ "-depth.png"))
+            print(depth_file)
+            colours = np.array(h5f["colors"])[...,::-1].copy()
+            cv2.imwrite(colour_file,colours)
+            with open((depth_file), 'wb') as im:
+                float_arr = np.array(h5f["depth"])
+                mask = np.array(h5f["depth"]) < 100
+                int_arr = float_arr*mask*10000
+                writer = png.Writer(width=640, height=480, bitdepth=16, greyscale=True)
+                writer.write(im, int_arr.astype(np.int16))
+        shutil.rmtree(os.path.join(out_subfolder, str(num)))
 
     return count+1
 
@@ -93,10 +105,11 @@ if __name__ == "__main__":
     for num, name in enumerate(data_paths):
         data_paths[num] = os.path.join(data_folder, name)
     for num, name in enumerate(model_paths):
-        model_paths[num] = os.path.join(data_folder, name, "textured.obj")
+        model_paths[num] = os.path.join(model_folder, name, "textured.obj")
 
     if os.path.exists(out_folder):
-        shutil.rmtree(out_folder)
+        pass
+        # shutil.rmtree(out_folder)
     else:
         os.mkdir(out_folder)
 
